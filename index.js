ceb27f5140b0c21e7c6d261aecf312ec864212ab
unknown <kamleshchandel04@gmail.com>
2024-03-03 19:17:07 +0530

    create user commands

--- index.js
+++ index.js
@@ -9,12 +9,19 @@ const client = new Client({
     ],
 });
 
+//Normal Message will be handled here
 client.on("messageCreate", (message) => {
     if (message.author.bot) return;
     message.reply({
-        content: "Hello from Kamlesh, This is a Bot.",
+        content: `Hello ${message.author.globalName}\nHow can I help you ?`,
     });
 });
 
+//Interactive Message will be handles here "/ping"
+client.on('interactionCreate' , (interaction) =>{
+    console.log(interaction);
+    interaction.reply("Pong!!");
+})
+
 dotenv.config();
-client.login(process.env.TOKEN, console.log(`Server Running`));
+client.login(process.env.TOKEN, console.log(`Bot Started`));
